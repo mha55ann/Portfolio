585db271263a4f0b83220372e666fa8017c38f00
MUHAMMAD HASSAN <ha55ansmail525@gmail.com>
2025-08-23 12:01:02 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
-import Services from "./components/Services";
 import Projects from "./components/Projects";
 
 function App() {
