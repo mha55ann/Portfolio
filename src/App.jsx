69e72314e51a97ec88ac5875e9f4d8db3b6b9c22
MUHAMMAD HASSAN <ha55ansmail525@gmail.com>
2026-08-29 09:56:01 +0500

    Update: Add Research and Certificates components to App navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import TechStack from "./components/TechStack";
+import Research from "./components/Research";
+import Certificates from "./components/Certificates";
 import Experience from "./components/Experience";
-import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Navbar />
       <Home />
       <TechStack />
+      <Research />
+      <Certificates />
       <Experience />
       <Projects />
       <Education />
