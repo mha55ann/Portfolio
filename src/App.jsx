2cc262cb3938ea44659603cec418dded34a53c98
MUHAMMAD HASSAN <ha55ansmail525@gmail.com>
2026-08-29 10:08:59 +0500

    Reorganize: Move Tech Stack below Certificates, reorder Research and Certificates first

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import TechStack from "./components/TechStack";
 import Research from "./components/Research";
 import Certificates from "./components/Certificates";
+import TechStack from "./components/TechStack";
 import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <TechStack />
       <Research />
       <Certificates />
+      <TechStack />
       <Experience />
       <Projects />
       <Education />
